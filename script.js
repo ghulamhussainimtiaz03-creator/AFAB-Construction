bb6d05d64bd143caa79f1384ce0d2a9edd780ddd
ghulamhussainimtiaz03-creator <ghulamhussainimtiaz03@gmail.com>
2026-09-23 13:36:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('year').textContent = new Date().getFullYear();
+  const navToggle = document.getElementById('navToggle');
+  const navLinks = document.getElementById('navLinks');
+  navToggle.addEventListener('click', () => navLinks.classList.toggle('open'));
+  navLinks.querySelectorAll('a').forEach(a => a.addEventListener('click', () => navLinks.classList.remove('open')));
